52175016c7960f55a1a2009dc0fec1868d8bb6f5
Hamo Atayan <hmayak.atayan@gmail.com>
2021-04-13 20:11:03 +0400

    asdasd

---
+++ src/main/java/com/Besharp/java/javalessons/AAAA.java
@@ -0,0 +1,10 @@
+package com.Besharp.java.javalessons;
+
+/**
+ * Created by Hmayak Atayan on 13, Apr, 2021
+ */
+public class AAAA {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/Besharp/java/javalessons/asd.java
@@ -0,0 +1,7 @@
+package com.Besharp.java.javalessons;
+
+/**
+ * Created by Hmayak Atayan on 13, Apr, 2021
+ */
+public class asd {
+}

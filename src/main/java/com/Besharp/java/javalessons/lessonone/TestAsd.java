3551b821a2af2300cd9c600536f09f8b23905405
hmayak <hmayak.atayan@gmail.com>
2021-04-13 20:46:43 +0400

    add aaa5

---
+++ src/main/java/com/Besharp/java/javalessons/lessonone/TestAsd.java
@@ -0,0 +1,10 @@
+package com.Besharp.java.javalessons.lessonone;
+
+/**
+ * Created by Hmayak Atayan on 13, Apr, 2021
+ */
+public class TestAsd {
+    public static void main(String[] args) {
+        System.out.println("this is updated version");
+    }
+}
--- src/main/java/com/Besharp/java/javalessons/lessontwo/asd2.java
+++
@@ -1,7 +0,0 @@
-package com.Besharp.java.javalessons.lessontwo;
-
-/**
- * Created by Hmayak Atayan on 13, Apr, 2021
- */
-public class asd2 {
-}

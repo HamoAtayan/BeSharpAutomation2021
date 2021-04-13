12441ab775caad84e4bf81c003916e7f276c6dbf
Hamo Atayan <hmayak.atayan@gmail.com>
2021-04-13 18:25:54 +0400

    first commit

---
+++ src/main/java/com/Besharp/java/TestClass.java
@@ -0,0 +1,10 @@
+package com.Besharp.java;
+
+/**
+ * Created by Hmayak Atayan on 13, Apr, 2021
+ */
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}

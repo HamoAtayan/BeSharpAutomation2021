760b56aa202e8f955bfffa88c8a75473a5d28fb4
tikseyranyan@gmail.com <tikseyranyan@gmail.com>
2021-04-13 21:58:31 +0400

    astana

--- src/main/java/com/Besharp/java/TestClass.java
+++ src/main/java/com/Besharp/java/TestClass.java
@@ -5,6 +5,6 @@ package com.Besharp.java;
  */
 public class TestClass {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("TestTTT");
     }
 }

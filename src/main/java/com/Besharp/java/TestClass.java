14cbb26d341d37d98427d7852ec523153b9eb4f4
tikseyranyan@gmail.com <tikseyranyan@gmail.com>
2021-04-20 21:42:43 +0400

    hashvel enq value-n

--- src/main/java/com/Besharp/java/TestClass.java
+++ src/main/java/com/Besharp/java/TestClass.java
@@ -1,10 +1,20 @@
 package com.Besharp.java;
 
+import java.util.Scanner;
+
 /**
  * Created by Hmayak Atayan on 13, Apr, 2021
  */
 public class TestClass {
     public static void main(String[] args) {
-        System.out.println("TestTTT");
+        Scanner TT = new Scanner(System.in);
+        System.out.print("number of hours worked: ");
+        int a = TT.nextInt();
+        System.out.print("hourly pay rent: ");
+        int b = TT.nextInt();
+        int c = a*b;
+        System.out.print("total value: ");
+        System.out.println(c);
+
     }
 }
